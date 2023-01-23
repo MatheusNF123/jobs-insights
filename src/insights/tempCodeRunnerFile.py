07dbe825655f699da3ea429452049e3890f8c995
Matheus <matheus_cs.nf@hotmail.com>
2023-01-23 15:37:51 -0300

    func get_unique_job_types retornando uma list dos job filtrados

--- src/insights/tempCodeRunnerFile.py
+++ src/insights/tempCodeRunnerFile.py
@@ -15,7 +15,7 @@ def read(path: str) -> List[Dict]:
 def get_unique_job_types(path: str) -> List[str]:
     list_jobs = read(path)
     job_types = [jobs['job_type'] for jobs in list_jobs]
-    return list(set(job_types))
+    print(list(set(job_types)))
     """Checks all different job types and returns a list of them
 
     Must call `read`
@@ -32,20 +32,4 @@ def get_unique_job_types(path: str) -> List[str]:
     """
     # raise NotImplementedError
 
-
-def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+get_unique_job_types('data/jobs.csv')
