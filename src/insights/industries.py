3151d8ba1b9d4fd54e4438d5150ad29481de4424
Matheus <matheus_cs.nf@hotmail.com>
2023-01-23 18:15:02 -0300

    func filter by industry retornar uma lista de dict filtrado

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -4,12 +4,18 @@ import src.insights.jobs as jobs
 
 def get_unique_industries(path: str) -> List[str]:
     list_jobs = jobs.read(path)
-    industry = [item['industry'] for item in list_jobs if item['industry']]
+    industry = [item["industry"] for item in list_jobs if item["industry"]]
     return list(set(industry))
     # raise NotImplementedError
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
+    filter_industry = [
+        industry_filter
+        for industry_filter in jobs
+        if industry_filter["industry"] == industry
+    ]
+    return filter_industry
     """Filters a list of jobs by industry
 
     Parameters
