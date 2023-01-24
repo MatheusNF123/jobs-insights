38cb5a8fdf178b27b1e13b5e9728b7610a027af7
Matheus <matheus_cs.nf@hotmail.com>
2023-01-23 23:16:10 -0300

    desenvolvendo test para func test_counter e test_brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    dicttest = {"salary": "2000", "title": "Maquinista", "type": "trainee"}
+    assert (
+        read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0] == dicttest
+    )
+    assert len(read_brazilian_file("tests/mocks/brazilians_jobs.csv")) == 15
+
+    try:
+        read_brazilian_file("tests/mocks/brazilians_jobsasd.csv")
+    except Exception as e:
+        assert e
